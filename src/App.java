d8c4d71479f3f71e207b6a88faf6c685ee3d8649
Fernando caguana <fernandocaguana31@gmail.com>
2026-07-26 20:41:47 -0500

    Nuevo

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import view.MainFrame;
 
 public class App {
     public static void main(String[] args) {
-        // Arranca la ventana principal de la aplicación
+        // Arranca la aplicación
         SwingUtilities.invokeLater(() -> {
             new MainFrame().setVisible(true);
         });
